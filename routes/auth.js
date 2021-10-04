96c3842acf911f26d0b0f3c2bf1113193d25bba5
Jose Fernando <jofer0628@gmail.com>
2021-10-04 16:53:57 -0500

    Google Sign added

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,28 @@
-const {Router} = require('express');
-const { check } = require('express-validator');
+const { Router } = require("express");
+const { check } = require("express-validator");
 
-const {login} = require('../controllers/authController');
-const { validarCampos } = require('../middlewares/validar-campos');
+const { login, googleSignIn } = require("../controllers/authController");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
-router.post('/login',[
-    check('correo','El correo es obligatorio').isEmail(),
-    check('password','La contraseña es obligatoria').not().isEmpty(),
-    validarCampos
-],login);
+router.post(
+  "/login",
+  [
+    check("correo", "El correo es obligatorio").isEmail(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    validarCampos,
+  ],
+  login
+);
+
+router.post(
+  "/google",
+  [
+    check("id_token", "id_token de google es necesario").not().isEmpty(),
+    validarCampos,
+  ],
+  googleSignIn
+);
 
 module.exports = router;
