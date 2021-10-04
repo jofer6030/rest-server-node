10ebdbf65bd90bdf23628f539b9d4f400f5633e1
Jose Fernando <jofer0628@gmail.com>
2021-10-03 23:46:33 -0500

    secret key changed

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,49 +1,46 @@
-const{response, request} = require('express')
-const jwt = require('jsonwebtoken')
+const { response, request } = require("express");
+const jwt = require("jsonwebtoken");
 
-const Usuario = require('../models/usuario')
+const Usuario = require("../models/usuario");
 
+const validarJWT = async (req = request, res = response, next) => {
+  const token = req.header("x-token");
 
-const validarJWT = async(req = request, res = response, next) => {
-    const token = req.header('x-token');
+  if (!token) {
+    return res.status(401).json({
+      msg: "No hay token en la petición",
+    });
+  }
 
-    if(!token){
-        return res.status(401).json({
-            msg: 'No hay token en la petición'
-        })
-    };
+  try {
+    const { uid } = jwt.verify(token, process.env.SECRETOPRIVATEKEY);
 
-    try {
-        
-        const {uid} = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-        
-        //leer al usuario que corresponde al uid
-        const usuario = await Usuario.findById(uid);
+    //leer al usuario que corresponde al uid
+    const usuario = await Usuario.findById(uid);
 
-        if(!usuario) {
-            return res.status(401).json({
-                msg: 'Token no válido - usuario no existe en BD'
-            })
-        }
-
-        //verificar si el uuid tiene estado en true
-        if(!usuario.estado){
-            return res.status(401).json({
-                msg : 'Token no válido - usuario estado: false'
-            })
-        }
-        req.usuario = usuario;
-
-        next();
-    } catch (error) {
-        console.log(error);
-        res.status(401).json({
-            msg: 'Token no válido'
-        })
+    if (!usuario) {
+      return res.status(401).json({
+        msg: "Token no válido - usuario no existe en BD",
+      });
     }
-}
 
+    //verificar si el uuid tiene estado en true
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Token no válido - usuario estado: false",
+      });
+    }
+    req.usuario = usuario;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      msg: "Token no válido",
+    });
+  }
+};
 
 module.exports = {
-    validarJWT
-}
+  validarJWT,
+};
